b70d516b0814fe5585a1250ff18099d51372d68d
1002659 <baw0803@sk.com>
2017-11-01 11:41:59 +0900

    [Refere] Optional Type 체크

---
+++ Wooky83.playground/Pages/OptionalTypeEx.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//OK
+var str1: String = "s"
+//FAIL
+//var str2: String = nil
+//OK
+var str3: String! = "s"
+//OK
+var str4: String! = nil
+//OK
+var str5: String? = "s"
+//OK
+var str6: String? = nil
