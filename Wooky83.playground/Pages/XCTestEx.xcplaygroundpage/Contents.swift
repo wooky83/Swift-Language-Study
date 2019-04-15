a1c88c875a4d3fcd8d986199503161cb42204782
1002659 <baw0803@sk.com>
2019-04-15 16:05:54 +0900

    XCTest 추가

---
+++ Wooky83.playground/Pages/XCTestEx.xcplaygroundpage/Contents.swift
@@ -0,0 +1,56 @@
+//: [Previous](@previous)
+
+import Foundation
+import XCTest
+
+class Score {
+    private var score = 0
+    
+    func getScore() -> Int {
+        return score
+    }
+    
+    func incrementScore() {
+        score += 1
+    }
+    
+    func incrementScore(_ completion: @escaping (_ newScore: Int) -> Void) {
+        incrementScore()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+            completion(self.score)
+        })
+    }
+}
+
+class ScoreTests: XCTestCase {
+    var keeper: Score!
+    
+    override func setUp() {
+        super.setUp()
+        keeper = Score()
+    }
+    
+    func testInitialGetScore() {
+        XCTAssert(keeper.getScore() == 0)
+    }
+    
+    func testIncrementScore() {
+        keeper.incrementScore()
+        XCTAssert(keeper.getScore() == 1)
+    }
+    
+    func testAsyncIncrmentScore() {
+        let scoreExpectation = expectation(description: "Score Returned")
+        
+        keeper.incrementScore { score in
+            print("Good Luck")
+            scoreExpectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1.0) { error in
+            print(error)
+        }
+    }
+}
+
+ScoreTests.defaultTestSuite.run()
