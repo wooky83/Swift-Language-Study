de8412c012af4048ebab368754136568baa15fea
1002659 <baw0803@sk.com>
2019-10-02 10:36:44 +0900

    [Ref] Swift5.0에서 String 변경사항 확인

---
+++ Wooky83.playground/Pages/Swift5_0_String.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+var bestTrainingSite = #""What's your name? "Good""#
+print(bestTrainingSite)
+
+let t1 = "iOS"
+let t2 = "Android"
+
+let stringName = #""Good Luck" \#(t1), \#(t2)"#
+print(stringName)
