a4c74202a748025aa51096da48f483e473cd0ba1
1002659 <baw0803@sk.com>
2020-02-06 15:23:04 +0900

    Option Set

---
+++ Wooky83.playground/Pages/OptionSet.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+import Foundation
+
+struct ShippingOptions: OptionSet {
+    let rawValue: Int
+
+    static let nextDay    = ShippingOptions(rawValue: 1 << 0)
+    static let secondDay  = ShippingOptions(rawValue: 1 << 1)
+    static let priority   = ShippingOptions(rawValue: 1 << 2)
+    static let standard   = ShippingOptions(rawValue: 1 << 3)
+
+    static let express: ShippingOptions = [.nextDay, .secondDay]
+    static let all: ShippingOptions = [.express, .priority, .standard]
+}
+
